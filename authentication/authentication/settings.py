19b6041a939c9dd12a9ddd6818aff9c38049f65b
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2023-06-26 12:38:43 +0530

    first commit

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'samples',
-        'USER': 'root',
-        'PASSWORD': 'rahman1640',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
